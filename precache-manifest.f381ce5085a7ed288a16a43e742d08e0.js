04da067b5b71c308588b5690804e7d90e94ba359
Joshua Carew <joshua.carew@yahoo.com>
2020-09-11 13:44:42 +0100

    Updates

---
+++ precache-manifest.f381ce5085a7ed288a16a43e742d08e0.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e271b446fb35cb133ca538a2c1444ef",
+    "url": "/cpshandiworks/index.html"
+  },
+  {
+    "revision": "3030fcd7436aa250d6eb",
+    "url": "/cpshandiworks/static/css/main.354f89e2.chunk.css"
+  },
+  {
+    "revision": "89dbb9aa8e5f102f738b",
+    "url": "/cpshandiworks/static/js/2.6a4d89cd.chunk.js"
+  },
+  {
+    "revision": "d267e358fa3fa9da8794898caa85ff71",
+    "url": "/cpshandiworks/static/js/2.6a4d89cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3030fcd7436aa250d6eb",
+    "url": "/cpshandiworks/static/js/main.6854f97c.chunk.js"
+  },
+  {
+    "revision": "748d3141fc44af919896",
+    "url": "/cpshandiworks/static/js/runtime-main.4d8b2a4c.js"
+  },
+  {
+    "revision": "9a52b5edf5be9383f8bf229fe52e3894",
+    "url": "/cpshandiworks/static/media/bathroom1.9a52b5ed.jpg"
+  },
+  {
+    "revision": "a1501e8aec5093900b85b46e38c71919",
+    "url": "/cpshandiworks/static/media/bg-mob.a1501e8a.png"
+  },
+  {
+    "revision": "f3e2deb595fe0a579a1d8a969038b9ce",
+    "url": "/cpshandiworks/static/media/bg.f3e2deb5.png"
+  },
+  {
+    "revision": "93440150f4d38fa1f949cd9746d8e1c5",
+    "url": "/cpshandiworks/static/media/checkatrade.93440150.png"
+  },
+  {
+    "revision": "761b60d8eaae03ee33f4631b1baf8506",
+    "url": "/cpshandiworks/static/media/contacthead.761b60d8.jpg"
+  },
+  {
+    "revision": "377e1d405ba67004884c724f78613248",
+    "url": "/cpshandiworks/static/media/fireplace.377e1d40.jpg"
+  },
+  {
+    "revision": "0fad489b572511be81853c2761cfe77d",
+    "url": "/cpshandiworks/static/media/galleryhead.0fad489b.jpg"
+  },
+  {
+    "revision": "86e44640a3435a188dda5cd824c95f9c",
+    "url": "/cpshandiworks/static/media/garden.86e44640.jpg"
+  },
+  {
+    "revision": "f2bd7593cf59fd9d9d2aff587f2913c3",
+    "url": "/cpshandiworks/static/media/kitchen1.f2bd7593.jpeg"
+  },
+  {
+    "revision": "6ed3bb841031b599a857b107553a5042",
+    "url": "/cpshandiworks/static/media/kitchen2.6ed3bb84.jpeg"
+  },
+  {
+    "revision": "29715e04d18e9c621c77ad48904a2ca1",
+    "url": "/cpshandiworks/static/media/kitchen3.29715e04.jpeg"
+  },
+  {
+    "revision": "f9de001f387ef75621865a99ea32f143",
+    "url": "/cpshandiworks/static/media/main1.f9de001f.jpg"
+  },
+  {
+    "revision": "8caf6c1f0944659aadcbf18749664326",
+    "url": "/cpshandiworks/static/media/main2.8caf6c1f.jpg"
+  },
+  {
+    "revision": "377e1d405ba67004884c724f78613248",
+    "url": "/cpshandiworks/static/media/p10i1.377e1d40.jpg"
+  },
+  {
+    "revision": "29715e04d18e9c621c77ad48904a2ca1",
+    "url": "/cpshandiworks/static/media/p11i1.29715e04.jpg"
+  },
+  {
+    "revision": "57e6536bf07e521ba73c37fe60bbc39c",
+    "url": "/cpshandiworks/static/media/p12i1.57e6536b.jpg"
+  },
+  {
+    "revision": "61251bde048c2f1de53495de66d50ef8",
+    "url": "/cpshandiworks/static/media/p13i1.61251bde.jpg"
+  },
+  {
+    "revision": "ebaac016e0ee5b84f42006d7fe448d22",
+    "url": "/cpshandiworks/static/media/p14i1.ebaac016.jpg"
+  },
+  {
+    "revision": "f2bd7593cf59fd9d9d2aff587f2913c3",
+    "url": "/cpshandiworks/static/media/p15i1.f2bd7593.jpg"
+  },
+  {
+    "revision": "d9bd464987e7e56ffa0f91d507c026bf",
+    "url": "/cpshandiworks/static/media/p15i2.d9bd4649.jpg"
+  },
+  {
+    "revision": "ddcb72a9cd0807b859a97b947597e644",
+    "url": "/cpshandiworks/static/media/p16i1.ddcb72a9.jpg"
+  },
+  {
+    "revision": "2518abf79a695d4999f3b6b207b0bf18",
+    "url": "/cpshandiworks/static/media/p17i1.2518abf7.jpg"
+  },
+  {
+    "revision": "e3276d7813037bd30555642ec43c8bb1",
+    "url": "/cpshandiworks/static/media/p17i2.e3276d78.jpg"
+  },
+  {
+    "revision": "4e2c42b69220e217defc961d2887f2f1",
+    "url": "/cpshandiworks/static/media/p18i1.4e2c42b6.jpg"
+  },
+  {
+    "revision": "8f23b70d772c62c939c616b7d6152dba",
+    "url": "/cpshandiworks/static/media/p19i1.8f23b70d.jpg"
+  },
+  {
+    "revision": "453caac137a3911424d1e5f8fc98cbcf",
+    "url": "/cpshandiworks/static/media/p1i1.453caac1.jpg"
+  },
+  {
+    "revision": "dd4e85aa063502df12b4979f50abb60b",
+    "url": "/cpshandiworks/static/media/p1i2.dd4e85aa.jpg"
+  },
+  {
+    "revision": "bd5f8eecdaca57edb5f391936163ffec",
+    "url": "/cpshandiworks/static/media/p1i3.bd5f8eec.jpg"
+  },
+  {
+    "revision": "aa5539a48b88be1fc98c31a01cd417ab",
+    "url": "/cpshandiworks/static/media/p2i1.aa5539a4.jpg"
+  },
+  {
+    "revision": "05d5659ec578a00fefadc113544005f4",
+    "url": "/cpshandiworks/static/media/p2i2.05d5659e.jpg"
+  },
+  {
+    "revision": "9a52b5edf5be9383f8bf229fe52e3894",
+    "url": "/cpshandiworks/static/media/p3i1.9a52b5ed.jpg"
+  },
+  {
+    "revision": "947b3845efa20b6d48cd217215ab2459",
+    "url": "/cpshandiworks/static/media/p3i2.947b3845.jpg"
+  },
+  {
+    "revision": "903a0856ed5c78969f78ff1b0e18a17b",
+    "url": "/cpshandiworks/static/media/p3i3.903a0856.jpg"
+  },
+  {
+    "revision": "86e44640a3435a188dda5cd824c95f9c",
+    "url": "/cpshandiworks/static/media/p4i1.86e44640.jpg"
+  },
+  {
+    "revision": "c057228671e1545a0082ea514217510b",
+    "url": "/cpshandiworks/static/media/p4i2.c0572286.jpg"
+  },
+  {
+    "revision": "078aa14669c15a70c13458a027517843",
+    "url": "/cpshandiworks/static/media/p4i3.078aa146.jpg"
+  },
+  {
+    "revision": "ee7a3202f40229fb3a75bde5b5248559",
+    "url": "/cpshandiworks/static/media/p5i1.ee7a3202.jpg"
+  },
+  {
+    "revision": "6ed3bb841031b599a857b107553a5042",
+    "url": "/cpshandiworks/static/media/p6i1.6ed3bb84.jpg"
+  },
+  {
+    "revision": "5fe594476f1966f39b0b50e46caba998",
+    "url": "/cpshandiworks/static/media/p6i2.5fe59447.jpg"
+  },
+  {
+    "revision": "d42a3edb04c324278f1b26022ed5f80c",
+    "url": "/cpshandiworks/static/media/p7i1.d42a3edb.jpg"
+  },
+  {
+    "revision": "843193c659e9fee6aa64bb45ab03c3c9",
+    "url": "/cpshandiworks/static/media/p8i1.843193c6.jpg"
+  },
+  {
+    "revision": "31a1d79a6fba7b64b90961834dc50b1c",
+    "url": "/cpshandiworks/static/media/p9i1.31a1d79a.jpg"
+  },
+  {
+    "revision": "05d5659ec578a00fefadc113544005f4",
+    "url": "/cpshandiworks/static/media/patio.05d5659e.jpg"
+  },
+  {
+    "revision": "47532b1c731d92a3c1144b41ad315948",
+    "url": "/cpshandiworks/static/media/review1.47532b1c.png"
+  },
+  {
+    "revision": "552ee18421d53894a2fb6be7871e20e6",
+    "url": "/cpshandiworks/static/media/review2.552ee184.png"
+  },
+  {
+    "revision": "73f9f7a85780075489af3500c5de0ce2",
+    "url": "/cpshandiworks/static/media/servicehead.73f9f7a8.jpg"
+  },
+  {
+    "revision": "57e6536bf07e521ba73c37fe60bbc39c",
+    "url": "/cpshandiworks/static/media/window.57e6536b.jpg"
+  }
+]);
